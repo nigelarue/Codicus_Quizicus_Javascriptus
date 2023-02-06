0a397b9903a85ce48bf9640445b6f46cb83b5185
nigelarue <larue.nigel91@gmail.com>
2023-02-05 21:48:32 -0600

    updated javascript logic in quiz.js and in fact removed other two javascript files, as I preferred my script being in one location. updated some of the HTML and started styling css.

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,97 +1,234 @@
-// Import questions from script.js
-import questions from './script';
 // DOM Reference variables
-var startBtnEl = document.querySelector('#startbtn');
-var timer = document.querySelector('#timer');
-var questionsEl = document.querySelector('#questions');
-var choicesEl = document.querySelector('#choices');
-var submitBtnEl = document.querySelector('#submit-btn');
-var initialsEl = document.querySelector('#initials')
-var questionCard = document.querySelector('#question-card');
-var gameOverCard = document.querySelector('#gameover-card');
-var quizStartEl = document.querySelector('#start-quiz');
-var scoreCardEl = document.querySelector('#score-card');
-// Global Variables to keep track of the time remaining
-var timeLeft = 60;
+var startBtnEl = document.querySelector("#startbtn");
 
-// Variable to keep track of the interval ID
+// Global Variables for timer
+var timeLeft = 60;
 var intervalId;
 var currentQuestion = 0;
 
-// Functions: methods/actions/handlers
-
 // start button click event
-startBtnEl.addEventListener('click', function() {
-    // start timer
-    intervalId = setInterval(function() {
-        timeLeft--;
-        timer.innerHTML = timeLeft;
+startBtnEl.addEventListener("click", function () {
 
-        // ends quiz if timer reaches 0
-        if (timeLeft === 0) {
-            clearInterval(intervalId);
-            endQuiz();
-        }
-    }, 1000);
-    // display first question & hide the quiz start elements
-    quizStartEl.setAttribute('class', 'hide');
-    
-    displayQuestion();
+  // start timer
+  intervalId = setInterval(function () {
+
+    timeLeft--;
+    timer.innerHTML = timeLeft;
+    // ends quiz if timer reaches 0
+    if (timeLeft <= 0) {
+      clearInterval(intervalId);
+      displayGameOver();
+    }
+  }, 1000);
+
+  // display first question & hide the quiz start elements
+  var quizStartEl = document.getElementById("start-quiz");
+  if (quizStartEl) {
+    quizStartEl.classList.add("hidden");
+  }
+
+  displayQuestion();
 });
 
-// show the first question
+// displays the questions and their corresponding answers
+var choicesEl = document.querySelector("#choices");
+
 function displayQuestion() {
-    questionCard.classList.remove("hide");
-    let question = questions[currentQuestion];
-    document.getElementById("question-header").innerHTML = question.text;
-    let choices = question.choices;
-    for (let i = 0; i < choices.length; i++) {
-        let choice = choices[i];
-        let btn = document.createElement("button");
-        btn.innerHTML = choice;
-        btn.addEventListener("click", function () {
-            if (i === question.answer) {
-                currentQuestion++;
-                if (currentQuestion === questions.length) {
-                    // show 'game over card'
-                    displayGameOver();
-                } else {
-                    displayQuestion();
-                }
-            } else {
-                // decrease the time here/error
-                timeLeft -= 5;
-                time.textContent = timeLeft;
-            }
-        });
-        document.getElementById("choices").appendChild(btn);
-    }
+  var questionCard = document.getElementById("question-card");
+  questionCard.removeAttribute("class", "hidden");
+
+  var timer = document.getElementById("timer");
+  timer.removeAttribute("class");
+
+  let question = questions[currentQuestion];
+  document.getElementById("question-header").innerHTML = question.text;
+
+  let choices = question.choices;
+  document.getElementById("choices").innerHTML = "";
+
+  for (let i = 0; i < choices.length; i++) {
+
+    var choice = choices[i];
+    var btn = document.createElement("button");
+    btn.classList.add("questionBtn");
+    btn.innerHTML = choice;
+    btn.addEventListener("click", function () {
+
+      if (choice === question.answer) {
+
+        currentQuestion++;
+        if (currentQuestion === questions.length) {
+
+          displayGameOver();
+        } else {
+          displayQuestion();
+        }
+      } else {
+        timeLeft -= 5;
+        timer.textContent = timeLeft;
+        if (timeLeft <= 0) {
+          clearInterval(intervalId);
+          displayGameOver();
+        }
+      }
+    });
+
+    document.getElementById("choices").appendChild(btn);
+  }
 }
 
-function endQuiz() {
-    questionCard.setAttribute('class', 'hide');
-    
+// array of questions
+const questions = [
+  {
+    text: 'What does the abbreviation "DOM" stand for?',
+    choices: [
+      "Document Object Model",
+      "Data Oriented Module",
+      "Data Oriented Model",
+      "Document Oriented Module",
+    ],
+    answer: "Document Object Model",
+  },
+  {
+    text: 'What is a "Method" in JavaScript?',
+    choices: [
+      "Methods are functions stored as object properties",
+      "A JavaScript method is a property containing a function definition",
+      "Methods are object properties",
+      "A JavaScript method is a block of code written to perform some specific set of tasks",
+      'A JavaScript method is a collection of properties with key values that can either be a "data" property or an "accessor" property',
+    ],
+    answer:
+      "A JavaScript method is a property containing a function definition",
+  },
+  {
+    text: 'What is an "object" in JavaScript?',
+    choices: [
+      "a material item that can be seen and touched",
+      "a type of data that can store and manipulate text",
+      "a data type that can take in a different types of data values",
+      "a sequence of characters",
+    ],
+    answer: "a data type that can take in a different types of data values",
+  },
+  {
+    text: 'What is an "arity" in JavaScript?',
+    choices: [
+      "a script to shrink text to live inside larger objects",
+      "a type of data that will initialize a method",
+      "a property describing the number of arguments in a function",
+      "a function used to shrink an index",
+    ],
+    answer: "a property describing the number of arguments in a function",
+  },
+  {
+    text: "In JavaScript, ____ function is not identified by a name.",
+    choices: ["anonymous", "John Doe", "blank", "undefined"],
+    answer: "anonymous",
+  },
+  {
+    text: "In JavaScript, ____ is what you call functions, classes, or variables that are declared at the top of their scope prior to the execution of the code.",
+    choices: ["rigging", "holstering", "hosting", "hoisting"],
+    answer: "hoisting",
+  },
+  {
+    text: "In JavaScript, a ____ is what you call changes or modifications to DOM elements.",
+    choices: ["mutatant", "mutation", "mutual", "mantra"],
+    answer: "mutation",
+  },
+  {
+    text: "In JavaScript, and life, ____ is what you call the standard coding writing style practices without any flair or frameworks.",
+    choices: ["beige", "boring", "vanilla", "basic"],
+    answer: "vanilla",
+  },
+  {
+    text: "In JavaScript, a ____ is when you write the code to perform a specific task(s) repeatedly under certain conditions.",
+    choices: ["loop", "curl", "straw", "lobe"],
+    answer: "loop",
+  },
+  {
+    text: "In JavaScript, a ____ is when you write the code to perform a specific task(s) repeatedly under certain conditions.",
+    choices: ["loop", "curl", "straw", "lobe"],
+    answer: "loop",
+  },
+  {
+    text: 'Out of the below, which is a "String Method":',
+    choices: ["splice", "lint", "straw", "slice"],
+    answer: "slice",
+  },
+  {
+    text: "In order to get an HTML element via the ID and source it in JavaScript you would use a ___ method.",
+    choices: [
+      "getWreckedById()",
+      "getElementByIp()",
+      "getElementId()",
+      "getElementById()",
+    ],
+    answer: "getElementById()",
+  },
+];
+
+// displays the gameover card and the 
+function displayGameOver() {
+
+  clearInterval(intervalId);
+  var questionCard = document.getElementById("question-card");
+  var gameOverCard = document.getElementById("gameover-card");
+
+  if (gameOverCard) {
+    gameOverCard.classList.remove("hidden");
+  }
+
+  if (questionCard) {
+    questionCard.classList.add("hidden");
+  }
+
+  saveScores();
 }
 
-function displayGameOver () {
-    gameOverCard.classList.remove("hide");
-    saveScores();
+// storage & submit click event
+var scoreCardEl = document.getElementById("scoreCard");
+var saveScores = document.getElementById("saveScores");
+var submitBtnEl = document.getElementById("submitBtn");
+var saveScore;
+var saveInitials;
+
+function saveScores(event) {
+
+  event.preventDefault();
+  var saveInitials = initials.value;
+  if(localStorage.hasOwnProperty("saveScores")){
+
+    saveScores = JSON.parse(localStorage.getItem("saveScores"));
+    saveScores.push({
+      initials: saveInitials,
+      score: saveScore
+    });
+  } else {
+
+    saveScores = [{
+      initials: saveInitials,
+      score: saveScore
+    }];
+  }
+
+  localStorage.setItem("saveScores", JSON.stringify(saveScores));
+  displayScoreCardEl(); 
 }
 
+function displayScoreCardEl() {
 
-// storage
-function saveScores() {
-    var initials = initialsEl.value.trim();
-    var score = JSON.parse(window.localStorage.getItem("timeLeft"));
-    var newScore = {
-        score: time,
-        initials: initials,
-    };
-    score.push(newScore);
-    window.localStorage.setItem("scores", JSON.stringify(score));
+  storedScores = JSON.parse(localStorage.getItem("saveScores"));
+  for(var i =0; i<storedScores.length; i++) {
+
+    var newScore = document.createElement("li");
+    newScore.textContent = storedScores[i].initials + " " + storedScores[i].score;
+    scoreStoreList.appendChild(newScore);
+  }
+
+  scoreCardEl.classList.remove("hidden");
 }
 
-// click events
-submitBtnEl.onclick = saveScores;
+submitBtnEl.addEventListener("submit", saveScores);
 
-// END OF PROGRAM
+  
